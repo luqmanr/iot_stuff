290afc2a676c06540f4e2df42676949900e17719
luqmanr <luqman.rahardjo@gmail.com>
2023-02-14 18:40:09 +0700

    Create main.cpp

---
+++ ultrasonic/main.cpp
@@ -0,0 +1,59 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-hc-sr04-ultrasonic-arduino/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+#include "Arduino.h"
+
+// #define triggerPin  D8
+// #define echoPin     D7
+
+const int triggerPin = 15;
+const int echoPin = 13;
+
+//define sound velocity in cm/uS
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+void setup() {
+  Serial.begin(115200); // Starts the serial communication
+  pinMode(triggerPin, OUTPUT); // Sets the triggerPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+}
+
+void loop() {
+  // Clears the triggerPin
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the triggerPin on HIGH state for 10 micro seconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  // 0.34/2
+  distanceCm = duration * SOUND_VELOCITY/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+  
+  delay(1000);
+}
